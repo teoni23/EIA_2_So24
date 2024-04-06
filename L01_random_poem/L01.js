d003ec81dce1a2d95d7bd49d40682aabf1cbe4cb
Minh Hieu Tony Tran <147735456+teoni23@users.noreply.github.com>
2024-04-06 22:41:35 +0200

    L01_poem_overworked

--- L01_random_poem/L01.js
+++ L01_random_poem/L01.js
@@ -1,11 +1,11 @@
 "use strict";
 const pizaa = ["Margherita", "Pepperoni", "Vegetarian", "Hawaiian", "Quattro Stagioni", "Capricciosa"];
-const prädikat = ["mit", "am beschten", "dazu", "neben bei sowaws wie", "versüßt mit", "verschlingbar mit"];
+const praedikat = ["mit", "am beschten", "dazu", "neben bei sowaws wie", "versüßt mit", "verschlingbar mit"];
 const toppings = ["ein Santa shock", "UHU", "A4 Papier", "eine Tür", "Badminton Schläger", "ungesalzene Pommes"];
-function verse(pizza, prädikat, toppings) {
-    return `${zufälligesWort(pizza)} ${zufälligesWort(prädikat)} ${zufälligesWort(toppings)}`;
+function verse(pizza, praedikat, toppings) {
+    return `${zufaelligesWort(pizza)} ${zufaelligesWort(praedikat)} ${zufaelligesWort(toppings)}`;
 }
-function zufälligesWort(vonArray) {
+function zufaelligesWort(vonArray) {
     const index = Math.floor(Math.random() * vonArray.length);
     const gewähltesWort = vonArray[index];
     vonArray.splice(index, 1);
@@ -14,11 +14,10 @@ function zufälligesWort(vonArray) {
 function ausgabe() {
     let gedicht = "zufällig erstelltes gedicht:\n\n";
     for (let i = 0; i < 5; i++) {
-        const vers = verse(pizaa.splice(), prädikat.splice(), toppings.splice());
+        const vers = verse(pizaa, praedikat, toppings);
         gedicht += vers + "\n";
     }
     return gedicht;
 }
-let i = 2;
 console.log(ausgabe());
 //# sourceMappingURL=L01.js.map
--- L01_random_poem/L01.ts
+++ L01_random_poem/L01.ts
@@ -1,14 +1,14 @@
 
 
 const pizaa: string[] = ["Margherita", "Pepperoni", "Vegetarian", "Hawaiian", "Quattro Stagioni", "Capricciosa"];
-const prädikat: string[] = ["mit", "am beschten", "dazu", "neben bei sowaws wie", "versüßt mit", "verschlingbar mit"];
+const praedikat: string[] = ["mit", "am beschten", "dazu", "neben bei sowaws wie", "versüßt mit", "verschlingbar mit"];
 const toppings: string[] = ["ein Santa shock", "UHU", "A4 Papier", "eine Tür", "Badminton Schläger", "ungesalzene Pommes"];
 
-function verse(pizza: string[], prädikat: string[], toppings: string[]): string {
-    return `${zufälligesWort(pizza)} ${zufälligesWort(prädikat)} ${zufälligesWort(toppings)}`;
+function verse(pizza: string[], praedikat: string[], toppings: string[]): string {
+    return `${zufaelligesWort(pizza)} ${zufaelligesWort(praedikat)} ${zufaelligesWort(toppings)}`;
 }
 
-function zufälligesWort(vonArray: string[]): string {
+function zufaelligesWort(vonArray: string[]): string {
     const index = Math.floor(Math.random() * vonArray.length);
     const gewähltesWort = vonArray[index];
     vonArray.splice(index, 1);
@@ -18,7 +18,7 @@ function zufälligesWort(vonArray: string[]): string {
 function ausgabe(): string {
     let gedicht = "zufällig erstelltes gedicht:\n\n";
     for (let i = 0; i < 5; i++) {
-        const vers = verse(pizaa.splice(), prädikat.splice(), toppings.splice());
+        const vers = verse(pizaa, praedikat, toppings);
         gedicht += vers + "\n";
     }
     return gedicht;
